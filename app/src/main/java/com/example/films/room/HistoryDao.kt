2893a5d7be5cb8b10f0c98b73fda998e80f0ac27
salim-gb <salimalsado.gb@gmail.com>
2021-10-25 20:08:41 +0300

    init commit test

---
+++ app/src/main/java/com/example/films/room/HistoryDao.kt
@@ -0,0 +1,22 @@
+package com.example.films.room
+
+import androidx.room.*
+
+@Dao
+interface HistoryDao {
+
+    @Query("SELECT * FROM HistoryEntity")
+    fun all(): List<HistoryEntity>
+
+    @Query("SELECT * FROM HistoryEntity WHERE filmId LIKE :id")
+    fun getDataByWord(id: Long): List<HistoryEntity>
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    fun insert(entity: HistoryEntity)
+
+    @Update
+    fun update(entity: HistoryEntity)
+
+    @Delete
+    fun delete(entity: HistoryEntity)
+}
